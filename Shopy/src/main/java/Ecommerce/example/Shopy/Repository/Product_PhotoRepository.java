2266fed62f438715ea349837c9805387db562f08
HabibaaHossamAhmed <habibahossam.hh@gmail.com>
2024-12-10 15:18:37 +0200

    ControllersEnititiesReposCreation

--- Shopy/src/main/java/Ecommerce/example/Shopy/Repository/Product_PhotoRepository.java
+++
@@ -1,9 +0,0 @@
-package Ecommerce.example.Shopy.Repository;
-
-import Ecommerce.example.Shopy.Entity.Product_Photo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Product_PhotoRepository extends JpaRepository<Product_Photo, Long> {
-}
